3a492023ca7c53872d31e219e1547ed87dae0e4e
Pavlo GRITSENKO <pgritsen@e3r6p10.unit.ua>
2018-05-09 19:58:25 +0300

    Shorter!! Smaller!!

--- source.c
+++ source.c
@@ -6,32 +6,25 @@
 /*   By: pgritsen <pgritsen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 10:01:33 by exam              #+#    #+#             */
-/*   Updated: 2017/12/05 14:58:56 by pgritsen         ###   ########.fr       */
+/*   Updated: 2018/05/09 19:57:52 by pgritsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-long	find_brackets(char *s, char br)
+char	*find_brackets(char *s, char br)
 {
-	long	it;
-	long	tmp;
-
-	it = -1;
-	while (s[++it] && s[it] != br)
-	{
-		if (!br && (s[it] == ')' || s[it] == ']' || s[it] == '}'))
-			return (-1);
-		else if (s[it] == '(' && (tmp = find_brackets(&s[it + 1], ')')) == -1)
-			return (-1);
-		else if (s[it] == '{' && (tmp = find_brackets(&s[it + 1], '}')) == -1)
-			return (-1);
-		else if (s[it] == '[' && (tmp = find_brackets(&s[it + 1], ']')) == -1)
-			return (-1);
-		if (s[it] == '(' || s[it] == '[' || s[it] == '{')
-			it += tmp;
-	}
-	return (s[it] == br ? it + 1 : -1);
+	--s;
+	while (*++s && *s != br)
+		if (!br && (*s == ')' || *s == ']' || *s == '}'))
+			return (0);
+		else if (*s == '(' && (s = find_brackets(s + 1, ')')) == 0)
+			return (0);
+		else if (*s == '{' && (s = find_brackets(s + 1, '}')) == 0)
+			return (0);
+		else if (*s == '[' && (s = find_brackets(s + 1, ']')) == 0)
+			return (0);
+	return (*s == br ? s : 0);
 }
 
 int		main(int ac, char **av)
@@ -39,7 +32,7 @@ int		main(int ac, char **av)
 	if (ac == 1)
 		return (write(1, "\n", 1));
 	while (--ac && ++av)
-		if (find_brackets(*av, 0) == -1)
+		if (find_brackets(*av, 0) == 0)
 			write(1, "Error\n", 6);
 		else
 			write(1, "OK\n", 3);
